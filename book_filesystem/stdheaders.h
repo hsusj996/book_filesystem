67c050ad1f073f5c47ecce1049b4483c0ff48224
hsusj996 <tpwls1355@naver.com>
2022-10-01 20:19:14 +0900

    20221001 수정

--- book_filesystem/stdheaders.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define _CRT_SECURE_NO_WARNINGS		
-
-#include <stdio.h>		//기본
-#include <stdlib.h>		//malloc 등등
-#include <stdbool.h>	//true, false
-#include <string.h>		//strcmp, strtok등등
-#include <Windows.h>	//system 명령어 사용
